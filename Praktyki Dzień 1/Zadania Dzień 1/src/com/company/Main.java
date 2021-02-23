eb717b3e6570822b3801d575ad8f54b62d20375c
GoldenSlav <79305258+GoldenSlav@users.noreply.github.com>
2021-02-23 15:33:41 +0100

    Dodaje kod

---
+++ Praktyki Dzień 1/Zadania Dzień 1/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        //zadanie 1
+        int A = 77;
+        double B = 7.7;
+        char C = '!';
+        String D = "Mikołaj";
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+
+        //zadanie 2
+        int A2 = 5;
+        int B2 = 6;
+        int wynik = A2*A2+B2*B2;
+        System.out.println("Wynik = " + wynik);
+
+        //zadanie 3
+        int A3 = 7;
+        double pole = A3*A3 * Math.sqrt(3) / 4;
+        System.out.println("Pole  trójkąta równobocznego o boku 7 wynosi = " + pole);
+
+        //zadanie 4
+        String imie = "Mikołaj";
+        String nazwisko = " Szarowicz";
+        System.out.println(imie +  nazwisko);
+    }
+
+
+
+}
